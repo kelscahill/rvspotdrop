2874407084aa7ef5cfcb317a9e48d7cbe129ccb9
Kelsey Cahill <kcahill@southleft.com>
2021-11-10 17:49:33 -0500

    temp.

--- wp-content/plugins/wpforms-country-state-city-dynamic-dropdown/includes/countries-sql.php
+++
@@ -1,5 +0,0 @@
-<?php
-$country_insert="INSERT INTO $table_country (`id`, `name`) VALUES
-(1, 'United States'),
-(2, 'Canada')";
-?>
--- wp-content/plugins/wpforms-user-registration/assets/js/admin-users.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($){
-	$('tr:has(.submitapprove)').css('background-color', '#FFFFE0');
-	$('.actions select[name^="action"]').append(
-		'<option value="wpforms_bulk_approve">' + wpforms_admin_users.approve + '</option>' +
-		'<option value="wpforms_bulk_unapprove">' + wpforms_admin_users.unapprove + '</option>'
-	);
-});
